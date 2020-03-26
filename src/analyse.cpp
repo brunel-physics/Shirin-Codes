5d1e3e2021c5567b7df864b7f30b3ef5f678dc0d
Shirin Golpayegani <eepgssg@brunel.ac.uk>
2020-03-26 12:40:42 +0000

    Working version with btagging the logic still need checking

--- src/analyse.cpp
+++ src/analyse.cpp
@@ -1174,26 +1174,26 @@ void analyse(int argc, char* argv[])
         auto h_d_enu_events_btag_denom_PtVsEta = d_enu_top_selection.Histo2D({"MC btag_Pt_vs_eta_enu_Channel","MC btag pt Vs eta in electron-neutrino channel",50,0,400,50,-3,3},"btag_denom_pt","btag_denom_eta");
         auto h_d_enu_events_non_btag_denom_PtVsEta = d_enu_top_selection.Histo2D({"MC non btag_Pt_vs_eta_enu_Channel","MC non btag pt Vs eta in electron-neutrino channel",50,0,400,50,-3,3},"non_btag_denom_pt","non_btag_denom_eta");
 
-	auto BTaggedBinFunction{[&h_d_enu_events_btag_numer_PtVsEta, &h_d_enu_events_btag_denom_PtVsEta](const floats& DummyColumn, const floats& pts, const floats& etas){
+	auto BTaggedBinFunction{[&h_d_enu_events_btag_numer_PtVsEta, &h_d_enu_events_btag_denom_PtVsEta](const floats& pts, const floats& etas){
 		cout << "inside b tag function" << endl;
 		floats BTaggedEff{};
 		for(int i = 0; i < pts.size(); i++)
 		{
 
-			int PtNum = h_d_enu_events_btag_numer_PtVsEta->GetXaxis()->FindBin(pts.at(i));
-			int EtaNum = h_d_enu_events_btag_numer_PtVsEta->GetYaxis()->FindBin(etas.at(i));
-			int PtDenom = h_d_enu_events_btag_denom_PtVsEta->GetXaxis()->FindBin(pts.at(i));
-			int EtaDenom = h_d_enu_events_btag_denom_PtVsEta->GetYaxis()->FindBin(etas.at(i));
-			float Numerator = h_d_enu_events_btag_numer_PtVsEta->GetBinContent(&btag_numer_pt, &btag_numer_eta);
-			float Denominator = h_d_enu_events_btag_denom_PtVsEta->GetBinContent(&btag_denom_pt, &btag_denom_eta);
-			float eff = Numerator / Denominator;
+			float PtNum = h_d_enu_events_btag_numer_PtVsEta->GetXaxis()->FindBin(pts.at(i));
+			float EtaNum = h_d_enu_events_btag_numer_PtVsEta->GetYaxis()->FindBin(etas.at(i));
+			float PtDenom = h_d_enu_events_btag_denom_PtVsEta->GetXaxis()->FindBin(pts.at(i));
+			float EtaDenom = h_d_enu_events_btag_denom_PtVsEta->GetYaxis()->FindBin(etas.at(i));
+			//float Numerator = h_d_enu_events_btag_numer_PtVsEta->GetBinContent(&btag_numer_pt, &btag_numer_eta);
+			//float Denominator = h_d_enu_events_btag_denom_PtVsEta->GetBinContent(&btag_denom_pt, &btag_denom_eta);
+			float eff = (PtNum * EtaDenom) / (PtDenom * EtaNum);
 			BTaggedEff.push_back(eff);
 			cout<< "I have found the bins" << endl;
 		}
 		return BTaggedEff;
 	}};
 
-	auto d_enu_btag_eff = d_enu_top_selection.Define("EffBTagged", BTaggedBinFunction, {"DummyColumnBJet", "Jet_pt", "Jet_eta"}));
+	auto d_enu_btag_eff = d_enu_top_selection.Define("EffBTagged", BTaggedBinFunction, {"Jet_pt", "Jet_eta"});
 
 ///////////////////////////////////////////////////////////////////////// Muon Channel/////////////////////////////////////////////////////////////////////////////
         auto d_munu_event_selection = d.Define("tight_mus", is_good_tight_mu, {"Muon_isPFcand", "Muon_pt", "Muon_eta", "Muon_tightId", "Muon_pfRelIso04_all"})
