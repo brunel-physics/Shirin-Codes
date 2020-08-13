dec5e60654ab2ac5f678c9ba098d7c0176ac5561
Shirin Golpayegani <eepgssg@brunel.ac.uk>
2020-08-13 21:46:20 +0100

    middle of applying the riccardo methodology

--- src/TriggerSF.cxx
+++ src/TriggerSF.cxx
@@ -1,6 +1,30 @@
 //clang++ -Isrc -std=c++17 -march=native -pipe -Ofast -Wall -Wextra -Wpedantic -o build/tsf src/TriggerSF.cxx `root-config --libs` -lm
 
-// TODO :: find normalization factor for the DYJetsToLL
+// TODO :: 1. How to make sure 1 lepton is tight and 1 lepton is loose for the tight_lep_cut
+// TODO :: 2.Compute the Ns from the reports
+// TODO :: 3.divide data dist. / MC dist.
+
+
+// Tag and Probe Methodology:
+/*
+The idea of the tag and probe is to use Z->ll enriched samples
+(for MC, while for data I guess you get what you get) and try
+to reconstruct the Z mass through the double lepton invariant mass.
+One lepton is the tag, the other one is the probe. So you will
+have events where the reconstruction is successful (what I called
+“pass” before) and events where the probe didn’t pass your selection
+criteria, so this will go into a distribution of “fail”. Eventually,
+your distribution of “fail” will contain both background events,
+that is events where the probe wasn’t actually coming from a Z,
+and events where the probe was from a Z but just didn’t pass the selection.
+With a proper fit you can distinguish the former from the latter and obtain
+the number of good Z events which ended up in the fail distribution.
+Then the efficiency is simply:
+
+N(pass)/N(total), where N(total) = N(pass)+N(fail which were actually good Z events)
+
+You do this for data and for MC, you divide the efficiencies obtained and you get the SFs.
+*/
 
 #include <ROOT/RDataFrame.hxx>//#include <ROOT/RCsvDS.hxx>
 #include <TRandom3.h>// used Gaussian, uniform each once
@@ -41,8 +65,8 @@ enum channel     {elnu,munu};
   constexpr  float    MET_EL_PT   = 20.f;//80.f;// TODO: Need new values
   constexpr  float    MET_MU_PT   = 25.f;//40.f;
 
-  constexpr double     Z_MASS     =  91.1876;
-  constexpr double     Z_MASS_CUT =  20.    ;
+  constexpr double     Z_MASS     =  91.1876;*/
+  constexpr double     Z_MASS_CUT =  40.    ;/*
   constexpr double     W_MASS     =  80.385 ;
   constexpr double     W_MASS_CUT =  35.    ;
   constexpr double   TOP_MASS     = 172.5   ;
@@ -139,11 +163,12 @@ inline auto lep_sel(const channel ch){
 		}
 	};
 }
-inline auto lep_tight_cut(const channel ch, const dataSource ds){
+inline auto lep_tight_cut(const channel ch){
 	return [=](
 		 const   ints& mask
 		,const   ints& elids
 		,const floats& isos
+		,const   ints& chg // electric charge
 	){
 // TODO: In the case we want 1 loose lepton, comment the 4 NOTE lines below
 		bool result;
@@ -152,31 +177,24 @@ inline auto lep_tight_cut(const channel ch, const dataSource ds){
                 // for data (cms single lepton) we choose a tight
                 // lepton and apply a veto lepton (same channel)
 		// i.e. single electron has , 1 tight e and 1 veto e
-		 if(false) ;
+                ints   chmk   = chg[mask];
+		if(false) ;
 		 else if(elnu==ch){
 			ints   temp = elids[mask];
-			if(dy==ds){
-                        result = temp.size() == 1;// Choosing 1 Tight Lepton
-                        result = result && temp [0] >= EL_TIGHT_ID; // NOTE
-			}
-			else{
 			result = temp.size() == 2;// Choosing 2 Leptons
 			result = result && temp [0] >= EL_TIGHT_ID // NOTE
                                		&& temp [1] >= EL_LOOSE_ID // NOTE
-					&& temp [1] <  EL_TIGHT_ID;// NOTE
-			}
+					&& temp [1] <  EL_TIGHT_ID// NOTE
+					&& chmk [0] != chmk [1]
+					;
 		}else if(munu==ch){
 			floats temp = isos[mask];
-			if(dy==ds){
-                        result = temp.size() == 1;
-                        result = result && temp [0] <= MU_TIGHT_ISO;// NOTE
-			}
-			else{
 			result = temp.size() == 2;
 			result = result && temp [0] <= MU_TIGHT_ISO // NOTE
 			       		&& temp [1] <= MU_LOOSE_ISO // NOTE
-					&& temp [1] >  MU_TIGHT_ISO;// NOTE
-			}
+					&& temp [1] >  MU_TIGHT_ISO // NOTE
+					&& chmk [0] != chmk [1]
+					;
 		}else{throw std::invalid_argument(
 			"Unimplemented ch (lep_tight_cut)");}
 		return result;
@@ -204,56 +222,59 @@ template<typename T,typename U>
 template<typename T,typename U,typename... Types>
 [[gnu::const]] bool all_equal(const T& t, const U& u, Types const&... args)
 	{return t == u && all_equal(u, args...);}
-auto jet_lep_min_deltaR(
-	 const doubles &jet_etas
-	,const doubles &jet_phis
-	,const double   lep_eta
-	,const double   lep_phi
+auto find_z_pair(
+	 const doubles& pts
+	,const doubles& etas
+	,const doubles& phis
+	,const doubles& ms
+	,const    ints& ch //charge
 ){
-	if(!all_equal(jet_etas.size(),jet_phis.size()))
+	if(0<debug) std::cout<<"find z pair"<<std::endl;
+	// This function finds the pair nearest to z mass
+	double  z_reco_mass = std::numeric_limits<double>::infinity();
+	size_t  lep_index_1 = std::numeric_limits<size_t>::max();
+	size_t  lep_index_2 = std::numeric_limits<size_t>::max();
+	const size_t  nleps = pts.size();
+	if(!all_equal(njets,etas.size(),phis.size(),ms.size(),ch.size()))
 		throw std::logic_error(
-		      "Collections must be the same size (jet-lep dR)");
-	if(jet_phis.empty())
-		throw std::logic_error(
-		      "Collections must not be empty for (jet-lep dR)");
-	//if(0<debug) std::cout<<"jet_lep_min_dR"<<std::endl;
-	doubles min_dRs; min_dRs.reserve(jet_etas.size());
-	std::transform(
-		jet_etas.cbegin(),
-		jet_etas.  cend(),
-		jet_phis.cbegin(),
-		std::back_inserter(min_dRs),
-		[=](double jet_eta, double jet_phi){
-			return deltaR(jet_eta,jet_phi,lep_eta,lep_phi);});
-	return min_dRs;
+		"Collections must be the same size in Z-pair");
+	if(pts.size()==0)	throw std::logic_error(
+		"Collections must not be empty in Z-pair");
+	ints z_pair(nleps, 0);// vector of zeroes
+	for(size_t   i=0; i < nleps-1 ;++i)
+	for(size_t j=i+1; j < nleps   ;++j)
+		if(ch[i] != ch[j]){// Must be opposite sign lepton
+		ROOT::Math::PtEtaPhiMVector
+		lep1(pts[i],etas[i],phis[i],ms[i]),
+		lep2(pts[j],etas[j],phis[j],ms[j]);
+		if (const double reco_mass = (lep1+lep2).M();
+		std::abs(Z_MASS- reco_mass) < std::abs(Z_MASS-z_reco_mass)){
+		   z_reco_mass = reco_mass;// found nearer pair to z mass
+		   lep_index_1 = i;
+		   lep_index_2 = j;
+		}}
+	}
+	z_pair[lep_index_1] = 1;
+	z_pair[lep_index_2] = 1;
+	//if(1<debug) std::cout<<"z pair"<<z_pair<<std::endl;
+	return z_pair;
 }
-inline double transverse_w_mass(
-	 const double lep__pt
-	,const double lep_phi
-	,const float  met__pt
-	,const float  met_phi
+inline auto easy_mass_cut(const double theo,const double cut){
+               return [=](const double ours){return std::abs(ours-theo)<cut;}
+auto LVpairAdd(
+	 const doubles& pt__pair// Create LorentzV from 2 jets 4-mom
+	,const doubles& eta_pair
+	,const doubles& phi_pair
+	,const doubles& mas_pair
 ){
-	return 2.
-		* std::abs (std::sin( delta_phi(lep_phi
-		          - static_cast<double>(met_phi))*.5))
-		* std::sqrt(static_cast<double>(met__pt)
-		                              * lep__pt);
+	//if(0<debug) std::cout<<"LVpairAdd"<<std::endl;
+	if(2 != pt__pair.size()) throw std::logic_error(
+		"Not pair of Z (LVpairAdd)");
+	ROOT::Math::PtEtaPhiMVector
+	v(pt__pair[0],eta_pair[0],phi_pair[0],mas_pair[0]),
+	p(pt__pair[1],eta_pair[1],phi_pair[1],mas_pair[1]);
+	return v+p;
 }
-inline auto pile(
-	 const TH1D* const &PuWd
-	,const TH1D* const &PuUd
-	,const TH1D* const &PuDd
-){
-	return[=](const int npv){
-	std::map<puSf,double> dict = {{puW,1.},{upW,1.},{dnW,1.}};
-	dict[puW] = PuWd->GetBinContent(PuWd->GetXaxis()->FindBin(npv));
-	dict[upW] = PuUd->GetBinContent(PuUd->GetXaxis()->FindBin(npv));
-	dict[dnW] = PuDd->GetBinContent(PuDd->GetXaxis()->FindBin(npv));
-	if(0<debug)std::cout<<"pile "<<npv<<" "
-		<<dict[puW]<<" "<<dict[upW]<<" "<<dict[dnW]<<std::endl;
-	return dict;};
-}
-
 inline auto sf(
 	 const bool MC
 	,const TH1D* const &PuWd
@@ -272,8 +293,11 @@ inline auto sf(
 auto runLBfilter(
 	const std::map<size_t,std::vector<std::pair<size_t,size_t>>>
 	&runLBdict
+	,const bool MC)
 ){
 	return [&](const unsigned int run,const unsigned int LB){
+		bool dummy = true;
+		if(MC) return true;
 		auto search =  runLBdict.find(run);
 		if(  search == runLBdict.cend()) return false;// Not Found TODO: true?
 		if(std::any_of(search->second.cbegin(),
@@ -387,27 +411,11 @@ void TriggerSF ( const channel ch , const dataSource ds){
 	    " || Flag_eeBadScFilter"
 	       ,"Event Cleaning filter")
 	;
-	auto lumclean = [&,ds](){
-		switch (ds){
-			case dy :{auto  mc_clean = clean
-			          .Alias("Jet_pt","j_pt")
-				  ;
-				  return   mc_clean;break;}
-                        case cms:{auto cms_clean = clean
-			          .Alias("Jet_pt","j_pt")
-				  ;
-				  return  cms_clean;break;}
-                        default :throw std::invalid_argument(
-                                "Unimplemented ds (rdf set)");
-                }
-        }();
-	if(!MC){
-	lumclean
-	.Filter(runLBfilter(runLBdict),{"run","luminosityBlock"},
+	auto lumclean = clean
+	.Filter(runLBfilter(runLBdict,MC),{"run","luminosityBlock"},
 			   "LuminosityBlock filter")
 	;
-	}
-	auto tight = lumclean
+	auto offlep = lumclean
 	// lepton selection first
 	.Define("loose_leps",lep_sel(ch),{
 	        temp_header+"isPFcand"
@@ -419,28 +427,40 @@ void TriggerSF ( const channel ch , const dataSource ds){
 	       ,temp_header+"dxy"
 	       ,temp_header+"dz"
 	       })
-	.Filter(lep_tight_cut(ch,ds),{"loose_leps",
-	        "Electron_cutBased",// edit function for  tight -> loose
-	        "Muon_pfRelIso04_all"},"lepton cut")// left with 1 tight lepton
-	.Define("lep__pt","static_cast<double>("+temp_header+  "pt[loose_leps][0])")
-	.Define("lep_eta","static_cast<double>("+temp_header+ "eta[loose_leps][0])")
-	.Define("lep_phi","static_cast<double>("+temp_header+ "phi[loose_leps][0])")
-	.Define("tw_lep_mas",transverse_w_mass,
-	       {"lep__pt",
-	        "lep_phi","MET_pt","MET_phi"})
+        .Define("lep__pt","static_cast<doubles>("+temp_header+     "pt[loose_leps])")
+        .Define("lep_eta","static_cast<doubles>("+temp_header+    "eta[loose_leps])")
+        .Define("lep_phi","static_cast<doubles>("+temp_header+    "phi[loose_leps])")
+	.Define("lep_mas","static_cast<doubles>("+temp_header+   "mass[loose_leps])")
+	.Define("lep_chg","static_cast<doubles>("+temp_header+ "charge[loose_leps])")
+	.Define("z_reco_leps"       , find_z_pair,
+	       { lep__pt,
+	         lep_eta,
+	         lep_phi,//easier to push back
+	         lep_mas,
+		 lep_chg})
+	.Define(   "z_LV"         , LVpairAdd    ,
+	       {   "z_pair__pt"   ,
+	           "z_pair_eta"   ,
+	           "z_pair_phi"   ,
+	           "z_pair_mas"  })
+	.Define(   "z_mas"        ,"z_LV. M ()")
+	.Define(   "z__pt"        ,"z_LV.Pt ()")
+	.Define(   "z_eta"        ,"z_LV.Eta()")
+	.Filter( easy_mass_cut(Z_MASS,Z_MASS_CUT),{"z_mas"},"z mass cut")
 	// jets selection follows; lepton selected
-	.Define("rawJet_eta","static_cast<ROOT::RVec<double>>(Jet_eta )")
-	.Define("rawJet_phi","static_cast<ROOT::RVec<double>>(Jet_phi )")
-	.Define("jet_lep_min_dR"   ,jet_lep_min_deltaR,// later reused with doubles
-	       {"rawJet_eta","rawJet_phi","lep_eta","lep_phi"})// gcc fail template
 	.Define("sf",sf(MC,PuWd,PuUd,PuDd),{"PV_npvs"})
 	;
-	auto trig = tight
+	auto probe = offlep
 	.Filter(triggers(ch),
 		{ "HLT_Ele32_WPTight_Gsf_L1DoubleEG"
 		 ,"HLT_IsoMu27"
 		},"Triggers Filter")
 	;
+	auto  tag = probe
+        .Filter(lep_tight_cut(ch),{"loose_leps",
+        "Electron_cutBased",// edit function for  tight -> loose
+        "Muon_pfRelIso04_all",
+         temp_header+"charge"},"lepton cut")// left with 1 tight lepton
 
 	std::string opener;
 	switch(ch){
@@ -452,40 +472,29 @@ void TriggerSF ( const channel ch , const dataSource ds){
 	case  cms:{opener += "cms"          ;break;}
 	}
 	TFile tsf(("histo/tsf"+opener+".root").c_str(),"RECREATE");
-	auto origiPt = origi.Histo1D({
-	    "origiPt"       ,
-	    "Original P_{T}",
-	50,0,400},"lep_pts");
-	auto cleanPt = clean.Histo1D({
-	    "cleanPt"       ,
-	    "Cleaned P_{T}" ,
-	50,0,400},"lep_pts");
-	auto tightPt = tight.Histo1D({
-	    "tightPt"       ,
-	    "Tight P_{T} "  ,
-	50,0,400},"lep__pt" ,"sf");
-	tsf.WriteTObject(origiPt.GetPtr());tsf.Flush();sync();
-	tsf.WriteTObject(cleanPt.GetPtr());tsf.Flush();sync();
-	tsf.WriteTObject(tightPt.GetPtr());tsf.Flush();sync();
-	//if(MC){
-	trig.Report() ->Print();
-	auto  trigPt = trig.Histo1D({
-	    "trigPt"           ,
-	"Probe trigger P_{T} " ,
-	50,0,400},"lep__pt"     ,"sf");
-	tsf.WriteTObject( trigPt.GetPtr());tsf.Flush();sync();
-	/*}else{
-	auto   CMSdf = trig
-	.Filter(runLBfilter(runLBdict),{"run","luminosityBlock"},
-	       "LuminosityBlock filter")
-	;
-	CMSdf.Report() ->Print();
-	auto  trigPt = CMSdf.Histo1D({
-	    "trigPt"           ,
-	"Probe trigger P_{T} " ,
-	50,0,400},"lep__pt"     ,"sf");
-	tsf.WriteTObject( trigPt.GetPtr());tsf.Flush();sync();
-	}*/
+	auto ProbeZmass = probe.Histo2D({
+	    "probeZmass"     ,
+	    "probe Z\\text{mass GeV/}c^{2}" ,
+	50,60,150},"z_mas","sf");
+	TF1 *f1 = new TF1("f1","gaus",60,150);
+	f1->SetParameters(ProbeZmass->GetMaximum()
+			, ProbeZmass->GetMean()
+			, ProbeZmass->GetRMS() );
+	ProbeZmass->Fit("f1")
+
+	auto tagZmass  =  tag.Histo1D({
+	    "tagZmass"       ,
+	    "tag Z\\text{mass GeV/}c^{2}"   ,
+	50,60,150},"z_mas","sf");
+        TF1 *f2 = new TF1("f2","gaus",60,150);
+        f1->SetParameters(taZmass->GetMaximum()
+                        , tagZmass->GetMean()
+                        , tagZmass->GetRMS() );
+        tagZmass->Fit("f2")
+
+	tsf.WriteTObject(ProbeZmass.GetPtr());tsf.Flush();sync();
+	tsf.WriteTObject(  tagZmass.GetPtr());tsf.Flush();sync();
+	tag.Report() ->Print();
 	std::cout << "TriggerSF completed successfully" << std::endl;
 }
 int main ( int argc , char *argv[] ){
