76b8bc382678cf49967706a632b24b196953b2ed
Shirin Golpayegani <eepgssg@brunel.ac.uk>
2020-12-10 19:28:15 +0000

    plotall.cxx working plotmc.cxx not taking cmet stuff

--- plotall.cxx
+++ plotall.cxx
@@ -2,81 +2,84 @@
 #include <TCanvas.h>
 #include <TLegend.h>
 #include <THStack.h>
+#include <iterator>// just for std::size
 
 #include "src/tdrstyle.C"
 #include "src/calchisto.hpp"
 
-int plotmc(){
+std::string allNamesArray[][3] = {// histogram id, histogram title, x axis string
+	 //{"cmet_sEt_",   "Corrected MET " "sum E_{T} ","Sum E_{T} (GeV)"}
+	 { "met_sEt_","Un-corrected MET " "sum E_{T} ","Sum E_{T} (GeV)"}
+	//,{"cmet__pt_",   "Corrected MET "     "p_{T} ","p_{T} (GeV/c)"}
+	//,{"cmet_dpx_",   "Corrected MET #Delta p_{x} ","p_{x} (GeV/c)"}
+	//,{"cmet_dpy_",   "Corrected MET #Delta p_{y} ","p_{y} (GeV/c)"}
+	,{ "met__pt_","Un-corrected MET "     "p_{T} ","p_{T} (GeV/c)"}
+	,{"W_invariant_mass_","W invariant mass ","\\text{W m_{ } (GeV/}c^{2})"}
+	,{     "tWm_","Transverse W mass ","\\text{W m_{T} (GeV/}c^{2})"}
+	,{     "tTm_","Transverse T mass ","\\text{T m_{T} (GeV/}c^{2})"}
+	,{    "zmas_",    "Recon. Z mass ","\\text{Z m_{ } (GeV/}c^{2})"}
+	,{        "Z_W_Delta_Phi_",      "Z  W  #Delta#phi ",    "Z &  W  #Delta#phi (rad)"}
+	,{      "Z_MET_Delta_Phi_",      "Z MET #Delta#phi ",    "Z & MET #Delta#phi (rad)"}
+	,{"Z_pair_jets_Delta_Phi_","Z pair jets #Delta#phi ","Z pair jets #Delta#phi (rad)"}
+	,{"ev_w_", "Event Weight ","Weight"}
+};
+
+int plotall(){
 	gROOT->SetBatch(kTRUE);// no open canvas window
 	setTDRStyle();
-	TFile     cf("plots/MC.root","RECREATE");
+	TFile     cf("plots/plotall.root","RECREATE");
 	TCanvas canv("name to reset","title to reset",10,10,900,900);
 	TH1D   *hobj;
 	// now we open ALL the files
 	std::map<std::pair<channel,dataSource>,TFile*> hFd;
-	// Channel and dataSource taken, to map to a TFile pointer
-	for(channel ch:channelAll){
+	for(channel ch:{elnu}){//channelAll){//for(channel ch:channelAll){
 	std::string chN;
 	switch     (ch){
 		case elnu:  {chN ="elnu_";break;}
 		case munu:  {chN ="munu_";break;}
 	}
 	for(dataSource ds:dataSourceAll){
-	if(cms == ds || met == ds)continue;
 	std::string  opener  =  chN ;
 	switch  (ds){
 		case tzq:{opener += "tzq";break;}
 		case  ww:{opener += "_ww";break;}
 		case  wz:{opener += "_wz";break;}
 		case  zz:{opener += "_zz";break;}
-		case ttb:{opener += "ttb";break;}
 		case ttz:{opener += "ttz";break;}
+		case ttb:{opener += "ttb";break;}
 		case met:{opener += "met";break;}
 		case cms:{opener += "cms";break;}
 	}
 	hFd[std::make_pair(ch,ds)]
-		= new TFile(("histo/Result/" + opener + ".root").c_str());
+		= new TFile(("histo/" + opener + ".root").c_str());
 	}}// now we have a histogram file dictionary of all the files miahahaha
-	for(std::string sf:
-	{"sfi","sfj","p_ej","p_sfej","p_sf_i","btag_w"}){
-	std::string xAxisStr;
-	if(false);
-	else if(  "sfi"     == sf) xAxisStr = "sf_{i}";
-	else if(  "sfj"     == sf) xAxisStr = "sf_{j}";
-	else if("p_ej"      == sf) xAxisStr = "\\prod_{j} 1 -                e_{j}";
-	else if("p_sfej"    == sf) xAxisStr = "\\prod_{j} 1 - \\text{sf}_{j} e_{j}";
-	else if("p_sf_i"    == sf) xAxisStr = "\\prod_{i}     \\text{sf}_{i}      ";
-	else if("btag_w"    == sf) xAxisStr = "btag weight";
-	else if("cmet_sEt_" == sf) xAxisStr = "Sum E_{T} (GeV)";
-	else if("cmet__pt_" == sf) xAxisStr = "p_{T} (GeV/c)";
-	else if("cmet_dpx_" == sf) xAxisStr = "p_{x} (GeV/c)";
-	else if("cmet_dpy_" == sf) xAxisStr = "p_{y} (GeV/c)";
 
-	for(channel ch:channelAll){
+	for(size_t i=0; i < std::size(allNamesArray) ;++i){
+	for(channel ch:{elnu}){//channelAll){
 	std::string chN;
 	switch     (ch){
 		case elnu:  {chN ="elnu";break;}
 		case munu:  {chN ="munu";break;}
-	}
-	std::string                title = sf + " " + chN;
-	std::string  stname =(sf + "_" + chN).c_str() ;
+	}// switch
+	std::string   title = allNamesArray[i][1] + chN;
+	std::string  stname = allNamesArray[i][0] + chN;
 	canv.SetName(stname.c_str());canv.SetTitle(stname.c_str());
 	THStack stac(stname.c_str(),title.c_str());
 	for(dataSource ds:dataSourceAll){
 	std::string  opener  = chN + "_";
-	if(cms == ds || met == ds)continue;// NOTE: Skip since NOT Monte Carlo
 	int colour;
 	switch  (ds){
 		case tzq:{opener += "tzq";colour = 6;break;}// magenta
 		case  ww:{opener += "_ww";colour = 2;break;}// red
 		case  wz:{opener += "_wz";colour = 3;break;}// green
 		case  zz:{opener += "_zz";colour = 4;break;}// blue
-		case ttb:{opener += "ttb";colour = 7;break;}// cyan
 		case ttz:{opener += "ttz";colour = 5;break;}// yellow
+		case ttb:{opener += "ttb";colour = 7;break;}// cyan
 		case met:{opener += "met";colour = 9;break;}// violet
 		case cms:{opener += "cms";colour = 1;break;}// black
 	}
-	std::string    hobjN = sf + "_" + opener ;
+	std::string    hobjN = allNamesArray[i][0] + opener;
+	std::cout<<"hobjN is "<< hobjN<<std::endl;
 	hFd[std::make_pair(ch,ds)]->GetObject(hobjN.c_str(),hobj);
 	                              hobj->SetDirectory(nullptr);
 	if( cms == ds || met == ds )  hobj->SetLineColor( colour);
@@ -85,7 +88,7 @@ int plotmc(){
 	}// dataSource
 	canv .cd();// pick me to draw?
 	stac .Draw("HIST");// must draw before set axes
-	stac .GetXaxis()->SetTitle(xAxisStr.c_str());
+	stac .GetXaxis()->SetTitle(allNamesArray[i][2].c_str());
 	stac .GetYaxis()->SetTitle("Event");
 	canv .BuildLegend();
 	canv .Update();
@@ -94,7 +97,7 @@ int plotmc(){
 //	canv .SaveAs(("plots/" + stname + ".pdf" ).c_str());
 	canv .Clear();// clear rather than delete, reusable!
 	// stac will auto clean up since it is not new-ed
-	}}// channel, particle
+	}}// channel, p
 	for(auto &x : hFd){
 		x.second->Close(); x.second = nullptr;
 	}
